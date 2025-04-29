6a07e985e6acea236f67afc0c4593977f0dbb9de
Manish Bhuva <manish.bhuvait@gmail.com>
2025-04-29 12:54:59 +0530

    changes

--- enter.php
+++ enter.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 $startTime = microtime(true);
 $data = file_get_contents('/home/amx/Z/portal/PgAvfHpU.php');
 $link = mysqli_connect('localhost','amx',$data,'amx_portal');
